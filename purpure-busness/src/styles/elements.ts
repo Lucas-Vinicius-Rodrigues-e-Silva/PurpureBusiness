37ed5add018f950dd6fc903a73cbfda4c20c343e
Josiel Luz <josiel9511791722@gmail.com>
2022-11-01 17:24:17 -0300

    feat: add text styles, btn styles, imput styles

--- purpure-busness/src/styles/elements.ts
+++ purpure-busness/src/styles/elements.ts
@@ -1,67 +1,76 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { primaryColor, waveColors } from "./colors";
+import { Text } from "./text/text";
 
 export const FilledBtn = styled.button`
-    background-color: #7D02DD;
-    color: white;
+  background-color: ${primaryColor};
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
 
-    &&:hover {
-        cursor: pointer;
-        background-color: #8F00FF;
-    }
-`
+  &&:hover {
+    cursor: pointer;
+    background-color: ${waveColors.color2};
+  }
+`;
 
 export const OutlinedBtn = styled.button`
-    background-color: white;
-    border: 1px solid #7D02DD;
-    color: #7D02DD;
+  background-color: white;
+  border: 1px solid ${primaryColor};
+  color: ${primaryColor};
+  border-radius: 5px;
+  padding: 10px 20px;
 
-    &&:hover {
-        background-color: rgba(125,2,221,0.1);
-        cursor: pointer;
-    }
-`
+  &&:hover {
+    background-color: ${waveColors.color1};
+    cursor: pointer;
+  }
+`;
 export const Input = styled.input`
-    border: none;
-    background-color: #f2f2f2;
-    border-radius: 5px;
+  border: 1px solid transparent;
+  background-color: #f2f2f2;
+  border-radius: 5px;
+  padding: 10px 20px;
 
-    &&:focus {
-        outline: 1px solid black;
-    }
-`
+  &&:focus {
+    outline: 1px solid black;
+  }
+`;
 
-export const Title1 = styled.h1`
-    font-size: 15px;
-    font-weight: 900;
-    color: black;
-`
+export const Title1 = styled(Text)`
+  font-size: 15px;
+  font-weight: 900;
+  color: black;
+`;
 
-export const Title2 = styled.h2`
-    font-size: 12px;
-    font-weight: bold;
-    color: black;
-`
+export const Title2 = styled(Text)`
+  font-size: 12px;
+  font-weight: bold;
+  color: black;
+`;
 
-export const Title3 = styled.h3`
-    font-size: 12px;
-    font-weight: normal;
-    color: black;
-`
+export const Title3 = styled(Text)`
+  font-size: 12px;
+  font-weight: normal;
+  color: black;
+`;
 
-export const Headline = styled.p`
-    font-size: 10px;
-    font-weight: normal;
-    color: black;
-`
+export const Headline = styled(Text)`
+  font-size: 10px;
+  font-weight: normal;
+  color: black;
+`;
 
-export const HeadlineBold = styled.p`
-    font-size: 10px;
-    font-weight: bold;
-    color: black;
-`
+export const HeadlineBold = styled(Text)`
+  font-size: 10px;
+  font-weight: bold;
+  color: black;
+`;
 
-export const Small = styled.small`
-    font-size: 7px;
-    font-weight: normal;
-    color: black;
-`
+export const Small = styled(Text)`
+  font-size: 7px;
+  font-weight: normal;
+  color: black;
+`;
--- purpure-busness/src/styles/globalStyles.ts
+++ purpure-busness/src/styles/globalStyles.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Inter', sans-serif;
+    text-decoration: none; 
 }
 
-`
+`;
