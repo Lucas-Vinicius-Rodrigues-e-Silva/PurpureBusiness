74175ff5759c8f484a9a16617594d1c5a53108c1
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2022-11-01 10:35:32 -0300

    feat: style

--- purpure-busness/src/Pages/landingPage/index.tsx
+++ purpure-busness/src/Pages/landingPage/index.tsx
@@ -1,47 +1,49 @@
 import { Link } from "react-router-dom"
+import { LandingPageStyle } from "../../styles/landingPage"
 
 
 export const LandingPage = () => {
 
     return (
-        <main>
+        <LandingPageStyle>
             <header>
-                <p>PUR</p><span>PURE</span> <p>Business</p>
-                <button> <Link to={'/register'}>Começar Grátis</Link> </button>
+                <div>
+                    <p>PUR<span>PURE</span></p> 
+                    <p> Business</p>
+                </div>
+                <Link className="landingPageBtn1" to={'/register'}>Começar Grátis</Link>
             </header>
             <section>
                 <h2>
                     Toda simplicidade para o seu PURO DESEMPENHO empresarial.
                 </h2>
-                <div>
-                    <button> <Link to={'/login'}>Efetuar Login</Link> </button>
+                <div className="btnContainer">
+                    <Link className="landingPageBtn1" to={'/login'}>Efetuar Login</Link>
                     <span>ou</span>
-                    <button> <Link to={'/register'}>Criar sua conta</Link> </button>
+                    <Link className="landingPageBtn2" to={'/register'}>Criar sua conta</Link>
                 </div>
-            </section>
-            <section>
-                <h1>POR QUE UTILIZAR NOSSA SOLUÇÃO?</h1>
-            </section>
-            <section>
-                <div>
+                <div className="apresentation">
+                    <h1>POR QUE UTILIZAR NOSSA SOLUÇÃO?</h1>
+                </div>
+                <div className="valuesBoxes">
                     <h3>SIMPLICIDADE</h3>
                     <p>
                         Temos uma ferramenta totalmente intuitiva e dinâmica com propósito de agilizar seu dia a dia e apresentar resultados satisfatórios.
                     </p>
                 </div>
-                <div>
+                <div className="valuesBoxes">
                     <h3>AGILIDADE</h3>
                     <p>
                         Desenvlvido por quem prioriza a resolução das tarefas do dia a dia de forma ágil e de qualidade.
                     </p>
                 </div>
-                <div>
+                <div className="valuesBoxes">
                     <h3>TRANSPARÊNCIA</h3>
                     <p>
                         Temos o compromisso em oferecer nossos serviços, bem como nosso suporte de forma totalmente transparente.
                     </p>
                 </div>
             </section>
-        </main>
+        </LandingPageStyle>
     )
 }
