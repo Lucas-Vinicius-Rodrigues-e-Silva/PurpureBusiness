ec9091e8e03b99320aeea382e1d8a93f0046a407
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2022-11-03 11:36:21 -0300

    style: completed

--- purpure-busness/src/Pages/landingPage/index.tsx
+++ purpure-busness/src/Pages/landingPage/index.tsx
@@ -14,34 +14,38 @@ export const LandingPage = () => {
                 <Link className="landingPageBtn1" to={'/register'}>Começar Grátis</Link>
             </header>
             <section>
-                <h2>
-                    Toda simplicidade para o seu PURO DESEMPENHO empresarial.
-                </h2>
-                <div className="btnContainer">
-                    <Link className="landingPageBtn1" to={'/login'}>Efetuar Login</Link>
-                    <span>ou</span>
-                    <Link className="landingPageBtn2" to={'/register'}>Criar sua conta</Link>
+                <div className="container">
+                    <h2>
+                        Toda simplicidade para o seu <span>PURO DESEMPENHO</span> empresarial.
+                    </h2>
+                    <div className="btnContainer">
+                        <Link className="landingPageBtn1" to={'/login'}>Efetuar Login</Link>
+                        <span>ou</span>
+                        <Link className="landingPageBtn2" to={'/register'}>Criar sua conta</Link>
+                    </div>
                 </div>
                 <div className="apresentation">
                     <h1>POR QUE UTILIZAR NOSSA SOLUÇÃO?</h1>
                 </div>
-                <div className="valuesBoxes">
-                    <h3>SIMPLICIDADE</h3>
-                    <p>
-                        Temos uma ferramenta totalmente intuitiva e dinâmica com propósito de agilizar seu dia a dia e apresentar resultados satisfatórios.
-                    </p>
-                </div>
-                <div className="valuesBoxes">
-                    <h3>AGILIDADE</h3>
-                    <p>
-                        Desenvlvido por quem prioriza a resolução das tarefas do dia a dia de forma ágil e de qualidade.
-                    </p>
-                </div>
-                <div className="valuesBoxes">
-                    <h3>TRANSPARÊNCIA</h3>
-                    <p>
-                        Temos o compromisso em oferecer nossos serviços, bem como nosso suporte de forma totalmente transparente.
-                    </p>
+                <div className="containerValues">
+                    <div className="valuesBoxes">
+                        <h3>SIMPLICIDADE</h3>
+                        <p>
+                            Temos uma ferramenta totalmente intuitiva e dinâmica com propósito de agilizar seu dia a dia e apresentar resultados satisfatórios.
+                        </p>
+                    </div>
+                    <div className="valuesBoxes">
+                        <h3>AGILIDADE</h3>
+                        <p>
+                            Desenvlvido por quem prioriza a resolução das tarefas do dia a dia de forma ágil e de qualidade.
+                        </p>
+                    </div>
+                    <div className="valuesBoxes">
+                        <h3>TRANSPARÊNCIA</h3>
+                        <p>
+                            Temos o compromisso em oferecer nossos serviços, bem como nosso suporte de forma totalmente transparente.
+                        </p>
+                    </div>
                 </div>
             </section>
         </LandingPageStyle>
--- purpure-busness/src/styles/landingPage.ts
+++ purpure-busness/src/styles/landingPage.ts
@@ -2,20 +2,50 @@ import styled from "styled-components";
 
 export const LandingPageStyle = styled.main`
 
+    .landingPageBtn1 {
+        text-decoration: none;
+        border: 2px solid #7D02DD;
+        color: #7D02DD;
+        cursor: pointer;
+        padding: 5px;
+        height: 75%;
+        margin: 3px 0;
+    }
+
+    .landingPageBtn2 {
+        text-decoration: none;
+        border-radius: 5px;
+        color: white;
+        background-color: #7D02DD;
+    }
+
+    h3 {
+        padding: 10px 0;
+    }
+
     header {
+
+        padding: 0 15px;
 
         display: flex;
         justify-content: space-between;
 
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
-        height: 45px;
+        height: 50px;
 
         div {
             display: flex;
+            align-items: center;
 
             p {
+                font-size: 16px;
                 padding-right: 5px;
+                font-weight: 900;
+
+                span {
+                    color: #7D02DD;
+                }
             }
         }
     }
@@ -24,18 +54,45 @@ export const LandingPageStyle = styled.main`
 
         background-image: linear-gradient(to right, #7D00DD, white);
         text-align: center;
-        height: 90vh;
+        height: 100vh;
+
+        .container {
+            display: flex;
+            height: 30%;
+            justify-content: space-between;
+            padding: 0 15px;
+
+            h2 {
+                color: white;
+                font-size: 20px;
+                font-weight: normal;
+                margin-top: 35px;
+                text-align: left;
+                max-width: 300px;
+
+                span {
+                    font-weight: 600;
+                }
+            }
+        }
+
+        .containerValues {
+            display: flex;
+            justify-content: center;
+        }
 
        .apresentation {
             background-color: white;
-            height: 80px;
+            height: 100px;
 
             display: flex;
             align-items: center;
             justify-content: center;
 
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
             h1 {
-                margin-top: 10px;
+                font-weight: 900;
             }
        }
 
@@ -44,17 +101,35 @@ export const LandingPageStyle = styled.main`
             flex-direction: column;
             align-items: center;
             background-color: white;
-            width: 136px;
-            height: 77px;
-            margin: 0 auto;
+            width: 250px;
+            height: 150px;
+            margin: 35px 50px 0 0;
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+            .landingPageBtn1 {
+                height: 30px;
+                padding: 5px;
+                margin-top: 30px; 
+            }
+
+            .landingPageBtn2 {
+                height: 30px;
+                padding: 5px;
+            }
        }
 
        .valuesBoxes {
             border-radius: 10px;
             box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
             background-color: white;
-            margin: 15px;
+            width: 250px;
+            margin: 100px 70px 0;
             padding: 10px;
+
+            p {
+                width: 100%;
+                height: 100%;
+            }
        }
     }
 
