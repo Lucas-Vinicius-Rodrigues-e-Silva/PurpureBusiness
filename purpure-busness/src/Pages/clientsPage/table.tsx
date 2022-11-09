f02b3d47aef955d4c01c8d3d1009507d0582ce35
Felipe Holanda <felipe.nether@icloud.com>
2022-11-09 12:15:05 -0300

    Refactor code to impove quality

--- purpure-busness/src/Pages/clientsPage/table.tsx
+++ purpure-busness/src/Pages/clientsPage/table.tsx
@@ -21,43 +21,55 @@ export const Table = () => {
       <tbody>
         {clientsFilter.length
           ? clientsFilter.map((item, i) => (
-              <tr key={i}>
-                <td>{item.id}</td>
-                <td>{item.client_name}</td>
-                <td>
-                  <RiPencilFill
-                    className="icon edit_icon"
-                    id={`${item.id}`}
-                    onClick={(e) => editModalOpen(e.currentTarget.id)}
-                  />
-                  <AiFillDelete
-                    className="icon delete_icon"
-                    id={`${item.id}`}
-                    onClick={(e) => deleteModalOpen(e.currentTarget.id)}
-                  />
+            <tr key={i}>
+              <td>{item.id}</td>
+              <td>{item.client_name}</td>
+              <td>
+                <RiPencilFill
+                  className="icon edit_icon"
+                  id={`${item.id}`}
+                  onClick={(e) => editModalOpen(e.currentTarget.id)}
+                />
+                <AiFillDelete
+                  className="icon delete_icon"
+                  id={`${item.id}`}
+                  onClick={(e) => deleteModalOpen(e.currentTarget.id)}
+                />
+              </td>
+            </tr>
+          ))
+          : (
+            clients.length === 0 ? (
+              <tr>
+                <td id="tableNoneMessage" colSpan={3}>
+                  <h2>Nenhum cliente cadastrado!</h2>
+                  <p>Cadastre o seu primeiro cliente clicando no botão <span>Novo cliente</span> a cima!</p>
                 </td>
               </tr>
-            ))
-          : clients.map((item, i) => (
-              <tr key={i}>
-                <td>{item.id}</td>
-                <td>{item.client_name}</td>
-                <td>
-                  <button
-                    id={`${item.id}`}
-                    onClick={(e) => editModalOpen(e.currentTarget.id)}
-                  >
-                    Editar
-                  </button>
-                  <button
-                    id={`${item.id}`}
-                    onClick={(e) => deleteModalOpen(e.currentTarget.id)}
-                  >
-                    Apagar
-                  </button>
-                </td>
-              </tr>
-            ))}
+            )
+              : (clients.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.id}</td>
+                  <td>{item.client_name}</td>
+                  <td>
+                    <RiPencilFill
+                      className="icon edit_icon"
+                      id={`${item.id}`}
+                      onClick={(e) => editModalOpen(e.currentTarget.id)}
+                    >
+                      Editar
+                    </RiPencilFill>
+                    <AiFillDelete
+                      className="icon delete_icon"
+                      id={`${item.id}`}
+                      onClick={(e) => deleteModalOpen(e.currentTarget.id)}
+                    >
+                      Apagar
+                    </AiFillDelete>
+                  </td>
+                </tr>
+              )))
+          )}
       </tbody>
     </TableContent>
   );
